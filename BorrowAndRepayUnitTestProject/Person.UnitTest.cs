4b965b6ae8c5525bbf30e95a37bc0046dfa03857
littlehorseboy <littlehorseboy@gmail.com>
2020-02-26 23:51:49 +0800

    test: refactor: 命名更改

--- BorrowAndRepayUnitTestProject/Person.UnitTest.cs
+++ BorrowAndRepayUnitTestProject/Person.UnitTest.cs
@@ -6,64 +6,64 @@ namespace BorrowAndRepayUnitTestProject
     [TestClass]
     public class PersonUnitTest
     {
-        Person i;
-        Person friend;
+        Person XiaoMing;
+        Person XiaoHua;
 
         public void InitClass()
         {
-            i = new Person("小明", 0);
-            friend = new Person("小華", 10000);
+            XiaoMing = new Person("小明", 0);
+            XiaoHua = new Person("小華", 10000);
         }
 
         [TestMethod]
         public void PersonIMoneyZero()
         {
             InitClass();
-            Assert.AreEqual(i.Money, 0);
+            Assert.AreEqual(XiaoMing.Money, 0);
         }
 
         [TestMethod]
         public void PersonFriendMoneyTenThousand()
         {
             InitClass();
-            Assert.AreEqual(friend.Money, 10000);
+            Assert.AreEqual(XiaoHua.Money, 10000);
         }
         
         [TestMethod]
         public void IBorrowFromFriendTenThousand()
         {
             InitClass();
-            i.borrow(friend, 10000);
-            Assert.AreEqual(i.Money, 10000);
-            Assert.AreEqual(friend.Money, 0);
+            XiaoMing.borrow(XiaoHua, 10000);
+            Assert.AreEqual(XiaoMing.Money, 10000);
+            Assert.AreEqual(XiaoHua.Money, 0);
         }
 
         [TestMethod]
         public void IRepayForFriendTenThousand()
         {
             InitClass();
-            i.borrow(friend, 10000);
-            i.repay(friend, 10000);
-            Assert.AreEqual(i.Money, 0);
-            Assert.AreEqual(friend.Money, 10000);
+            XiaoMing.borrow(XiaoHua, 10000);
+            XiaoMing.repay(XiaoHua, 10000);
+            Assert.AreEqual(XiaoMing.Money, 0);
+            Assert.AreEqual(XiaoHua.Money, 10000);
         }
 
         [TestMethod]
         public void IBorrowFromFriendOverload()
         {
             InitClass();
-            i.borrow(friend, 9999999);
-            Assert.AreEqual(i.Money, 0);
-            Assert.AreEqual(friend.Money, 10000);
+            XiaoMing.borrow(XiaoHua, 9999999);
+            Assert.AreEqual(XiaoMing.Money, 0);
+            Assert.AreEqual(XiaoHua.Money, 10000);
         }
 
         [TestMethod]
         public void IRepayForFriendOverload()
         {
             InitClass();
-            i.repay(friend, 9999999);
-            Assert.AreEqual(i.Money, 0);
-            Assert.AreEqual(friend.Money, 10000);
+            XiaoMing.repay(XiaoHua, 9999999);
+            Assert.AreEqual(XiaoMing.Money, 0);
+            Assert.AreEqual(XiaoHua.Money, 10000);
         }
     }
 }
