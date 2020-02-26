e3234d8a4f568ced87892dbf545f99e232391492
littlehorseboy <littlehorseboy@gmail.com>
2020-02-26 23:42:47 +0800

    test: InitClass，每次測試 method 都重新建構乾淨的 class

--- BorrowAndRepayUnitTestProject/Person.UnitTest.cs
+++ BorrowAndRepayUnitTestProject/Person.UnitTest.cs
@@ -6,24 +6,33 @@ namespace BorrowAndRepayUnitTestProject
     [TestClass]
     public class PersonUnitTest
     {
-        readonly Person i = new Person("小明", 0);
-        readonly Person friend = new Person("小華", 10000);
+        Person i;
+        Person friend;
+
+        public void InitClass()
+        {
+            i = new Person("小明", 0);
+            friend = new Person("小華", 10000);
+        }
 
         [TestMethod]
         public void PersonIMoneyZero()
         {
+            InitClass();
             Assert.AreEqual(i.Money, 0);
         }
 
         [TestMethod]
         public void PersonFriendMoneyTenThousand()
         {
+            InitClass();
             Assert.AreEqual(friend.Money, 10000);
         }
         
         [TestMethod]
         public void IBorrowFromFriendTenThousand()
         {
+            InitClass();
             i.borrow(friend, 10000);
             Assert.AreEqual(i.Money, 10000);
             Assert.AreEqual(friend.Money, 0);
@@ -32,6 +41,8 @@ namespace BorrowAndRepayUnitTestProject
         [TestMethod]
         public void IRepayForFriendTenThousand()
         {
+            InitClass();
+            i.borrow(friend, 10000);
             i.repay(friend, 10000);
             Assert.AreEqual(i.Money, 0);
             Assert.AreEqual(friend.Money, 10000);
@@ -40,6 +51,7 @@ namespace BorrowAndRepayUnitTestProject
         [TestMethod]
         public void IBorrowFromFriendOverload()
         {
+            InitClass();
             i.borrow(friend, 9999999);
             Assert.AreEqual(i.Money, 0);
             Assert.AreEqual(friend.Money, 10000);
@@ -48,6 +60,7 @@ namespace BorrowAndRepayUnitTestProject
         [TestMethod]
         public void IRepayForFriendOverload()
         {
+            InitClass();
             i.repay(friend, 9999999);
             Assert.AreEqual(i.Money, 0);
             Assert.AreEqual(friend.Money, 10000);
