cd48861eef884e1306518a241d970777309da6c5
littlehorseboy <littlehorseboy@gmail.com>
2020-02-26 23:15:20 +0800

    test: 新增 test 專案

---
+++ BorrowAndRepayUnitTestProject/Person.UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BorrowAndRepayUnitTestProject
+{
+    [TestClass]
+    public class PersonUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
